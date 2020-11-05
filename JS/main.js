ea2386f57fea711bd8fbff997c54e1c769e2dc9b
Natalie East <eastnatalie1@gmail.com>
2020-11-05 16:57:06 -0600

    add javascript with html

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+console.log("hello from san antonio");
+alert("You just won a new car!");
+alert("I'm just playin', enter your e-mail");
+confirmed = confirm('Are you sure you want to do XYZ?');
+
+var userInput = prompt('Please type your first name:');
+console.log('first name: ' + userInput);
