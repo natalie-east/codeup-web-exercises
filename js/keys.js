7714851cd575063239d5ee47540ba584b5c137dc
Natalie East <eastnatalie1@gmail.com>
2020-12-10 17:00:52 -0600

    key deletion

--- js/keys.js
+++
@@ -1,6 +0,0 @@
-//this file is for saving API keys and to not to be pushed up to Github
-let mapboxToken = 'pk.eyJ1IjoibmFkYWxhYXkiLCJhIjoiY2tpamZ5d3VyMDJjNTJ5cGN2eXNnb2FtdyJ9.LuSs26rfUHtOTTsbvv3mlQ'
-
-
-
-
