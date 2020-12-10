2f93265675620f5cebecf400b5e2fc3aa8b5a8e5
Natalie East <eastnatalie1@gmail.com>
2020-12-10 16:17:28 -0600

    maps

---
+++ js/keys.js
@@ -0,0 +1,7 @@
+//this file is for saving API keys and to not to be pushed up to Github
+let mapboxToken = 'pk.eyJ1IjoibmFkYWxhYXkiLCJhIjoiY2tpajB4bGxlMTM4MzJzczV2bGZtNTQ3MiJ9.w7yI9Tu9maF2SaD7ljTIxA';
+
+
+
+
+
