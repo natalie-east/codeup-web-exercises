73362102a4531ef83275d3e6c47ad53462eb691f
Natalie East <eastnatalie1@gmail.com>
2020-12-15 20:28:03 -0600

    updates

--- js/for_loops.js
+++ js/for_loops.js
@@ -30,3 +30,41 @@ console.log(output);
 for (var i =100;i >=5 ;i-=5){
     console.log(i);
 }
+
+function getSleepHours(day){
+    if (day === 'monday'){
+        return 8;
+    }else {
+    (day === 'tuesday')
+        return 8;
+}if else {
+    (day === 'wednesday')
+        return 8;
+}else if {
+    (day === 'thursday')
+        return 8;
+}else if{
+    (day === 'friday')
+        return 8;
+}else if{
+    (day === 'saturday')
+        return 8;
+}else if{
+    (day === 'sunday')
+        return 8;
+}
+}
+
+
+
+let second = (['second','first', 'third', 'fourth', 'fifth'])
+let first =second[1]
+
+let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
+
+// Write your code below
+let alienShip = {
+    retreat(){
+        console.log(retreatMessage)
+    }
+};
